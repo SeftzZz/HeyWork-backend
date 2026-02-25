359d7cf089417646d9e8ecf42c6361a92cdaf96a
fpp255 <fpp255@gmail.com>
2026-02-26 03:53:13 +0700

    update I 260226

--- ws-server.js
+++ ws-server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Salam123!',
+  password: '',
   database: 'heywork',
   waitForConnections: true,
   connectionLimit: 10
