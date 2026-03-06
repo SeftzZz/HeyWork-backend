77a7b0165cfa7258082c2a82a27840a4f9b0dc74
SeftzZz <yerblues6@gmail.com>
2026-03-06 11:16:06 +0700

    update role menu

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -44,7 +44,7 @@ $routes->group('admin', ['filter' => 'auth'], function($routes) {
     $routes->post('users/get', 'Admin\Users::getById', ['filter' => 'role:admin']);
     $routes->post('users/update', 'Admin\Users::update', ['filter' => 'role:admin']);
     $routes->post('users/delete', 'Admin\Users::delete', ['filter' => 'role:admin']);
-    $routes->post('users/get-partner', 'Admin\Users::getPartner', ['filter' => 'role:admin']);
+    $routes->post('users/get-partner', 'Admin\Users::getPartner', ['filter' => 'role:admin,hotel_hr']);
 
     $routes->get('application', 'Admin\Application::index', ['filter' => 'role:admin,hotel_hr,hotel_fnb_service,hotel_fnb_production,hotel_fo,hotel_hk,hotel_gm']);
     $routes->post('application/datatable', 'Admin\Application::datatable', ['filter' => 'role:admin,hotel_hr,hotel_fnb_service,hotel_fnb_production,hotel_fo,hotel_hk,hotel_gm']);
